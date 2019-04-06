97683202d8579961443317d59b63bf54fd44fc7a
Noshi96 <38572172+Noshi96@users.noreply.github.com>
2019-04-06 22:07:48 +0200

    Add files via upload

---
+++ CHashLab3Aplikacja/CHashLab3Aplikacja/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CHashLab3Aplikacja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(453, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Szukaj plików";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 437);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wykonaj kopię";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(297, 265);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(469, 146);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(297, 225);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 228);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Wybrana ścieżka:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Zawartość katalogu:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "lab3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CHashLab3Aplikacja/CHashLab3Aplikacja/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+
+
+namespace CHashLab3Aplikacja
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        List<XmlNodeList> listOfFiles = new List<XmlNodeList> { };
+        List<string> listOfFilesStr = new List<string> { };
+        Boolean find = false;
+        Boolean find2 = false;
+        string pathToCsproj, pathOfSln, pathToFiles, sln, csprojFile, slnName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            makeFolder(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            richTextBox1.Text = string.Empty;
+            listOfFiles.Clear();
+            listOfFilesStr.Clear();
+            find2 = false;
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBox1.Text = fbd.SelectedPath;
+                string[] fileList = Directory.GetFiles(textBox1.Text);
+
+                foreach (string s in fileList)
+                {
+                    richTextBox1.Text += Path.GetFileName(s);
+                    slnName = Path.GetFileName(s);
+                    richTextBox1.Text += "\n";                  
+
+                    if (Path.GetExtension(s) == ".sln")
+                    {
+                        find = true;
+                        find2 = true;
+                        pathOfSln = s;
+                        pathToFiles = s.Substring(0, s.LastIndexOf('.')) + "\\";
+                    }
+                }
+                if(find == true)
+                {
+                    List<string> listOfStr = new List<string> { };
+                    MessageBox.Show("Znaleziono plik SLN.");
+                    sln = File.ReadAllText(pathOfSln);
+
+                    var reg = new Regex("\".*?\"");
+                    var matches = reg.Matches(sln);
+                    foreach(var item in matches)
+                    {
+                        listOfStr.Add(item.ToString());
+                    }
+                    csprojFile = listOfStr[2].Split('\\').Last().Trim('"');
+                    pathToCsproj = textBox1.Text + "\\" + listOfStr[2].Trim('"');
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.Load(pathToCsproj);
+                    listOfFiles.Add(doc.GetElementsByTagName("Compile"));
+                    listOfFiles.Add(doc.GetElementsByTagName("EmbeddedResource"));
+                    listOfFiles.Add(doc.GetElementsByTagName("None"));
+                    foreach (XmlNodeList file in listOfFiles)
+                    {
+                        foreach(XmlNode item in file)
+                        {
+                            richTextBox1.Text += item.Attributes["Include"].Value + "\n";
+                            listOfFilesStr.Add(item.Attributes["Include"].Value);
+                        }
+                    }
+                    richTextBox1.Text += Path.GetFileName(pathToCsproj);
+                    listOfFilesStr.Add(Path.GetFileName(pathToCsproj));
+                    find = false;                    
+                }
+                else
+                {
+                    MessageBox.Show("Nie znaleziono pliku SLN, podaj inną ścieżkę!");
+                }          
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void makeFolder(string path)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Podaj ścieżkę!");
+            }
+            else if(find2)
+            {
+                Directory.CreateDirectory(path + "\\kopia");
+                if (!Directory.EnumerateFiles(path + "\\kopia").Any())
+                {
+                    foreach (string s in listOfFilesStr)
+                    {
+                        File.Copy((pathToFiles + s), path + "\\kopia\\" + s.Replace("Properties\\", ""));
+                    }
+                    File.Copy(pathOfSln, path + "\\kopia\\" + slnName);
+                    MessageBox.Show("Wykonano pomyślnie kopie");
+                }
+                else
+                {
+                    MessageBox.Show("Folder już istnieje i są w nim pliki, proszę je usunąć!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie można wykonać kopii, nie znaleziono pliku .sln!");
+            }
+        }
+    }
+}
